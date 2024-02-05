2f6b9c1c00214bcc5f9f087a28fae079cddc25f2
JYLeeLYJ <JYLeeLYJ@qq.com>
2024-02-05 20:37:49 +0800

    fix: compiler db generation command

--- visualstudio_compiledb.cmake
+++ visualstudio_compiledb.cmake
@@ -2,18 +2,21 @@ include(FetchContent)
 
 FetchContent_Declare(
     clang-power-tools
-    GIT_REPOSITORY https://github.com/Caphyon/clang-power-tools.git 
-    GIT_TAG master
+    GIT_REPOSITORY https://github.com/JYLeeLYJ/clang-power-tools.git
+    GIT_TAG noclear-console
 )
-
 FetchContent_MakeAvailable(clang-power-tools)
 
-function(generate_compiledb_in_visualstudio_build) 
+macro(generate_compiledb_in_visualstudio_build) 
     if(CMAKE_GENERATOR MATCHES "Visual Studio")
         execute_process(
-            COMMAND "powershell.exe ${clang-power-toolsSOURCE_DIR}/ClangPowerTools/ClangPowerToolsShared/Tooling/v1/clang-build.ps1 -export-jsondb"
+            COMMAND powershell.exe ${clang-power-tools_SOURCE_DIR}/ClangPowerTools/ClangPowerToolsShared/Tooling/v1/clang-build.ps1 -export-jsondb -dir ${PROJECT_BINARY_DIR}/${PROJECT_NAME}.sln 
             WORKING_DIRECTORY  ${PROJECT_BINARY_DIR}
             COMMAND_ECHO STDOUT
-        )    
+            OUTPUT_FILE clang-power-tools.log
+            COMMAND_ERROR_IS_FATAL ANY
+        )
+    else()
+        message(STATUS "skip generate compiledb on None-VS build") 
     endif()
-endfunction()
+endmacro()
